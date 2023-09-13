9666f9e0c727ad281e62613215ed46336def5878
garenpham <tanphamminh2002@gmail.com>
2023-09-12 18:25:17 -0700

    welcome mobile view

--- components/Home/Welcome/Gallery/Gallery.tsx
+++ components/Home/Welcome/Gallery/Gallery.tsx
@@ -6,18 +6,18 @@ type Props = {}
 
 function Gallery({}: Props) {
   return (
-    <div className='flex items-center gap-[12px]'>
-      <div className={`flex flex-col items-end gap-[12px]`}>
-        <div className={`flex items-center gap-[23px]`}>
+    <div className='flex items-center gap-[7px] lg:gap-[12px]'>
+      <div className={`flex flex-col items-end gap-[7px] lg:gap-[12px]`}>
+        <div className={`flex items-center gap-[12px] lg:gap-[23px]`}>
           <Image
             alt='icon'
             src='https://res.cloudinary.com/dwqpulybv/image/upload/v1694399641/shared-assets/landing%20page/pic1_hjqxmy.svg'
             height={52}
             width={50}
-            className='mt-[2rem]'
+            className='mt-[2rem] w-[25px] h-[26px] lg:w-[50px] lg:h-[52px]'
           />
 
-          <div className={`w-[125px] h-[166px]`}>
+          <div className={`w-[77px] h-[102px] lg:w-[125px] lg:h-[166px]`}>
             <Picture
               src='https://res.cloudinary.com/dwqpulybv/image/upload/v1694399395/shared-assets/landing%20page/69fa157a5a4a4e9622028b2b1b6bdc9dfa5f2cdb_b0nakj.png'
               className='rounded-[10px]'
@@ -25,7 +25,7 @@ function Gallery({}: Props) {
           </div>
         </div>
 
-        <div className={`w-[190px] h-[242px]`}>
+        <div className={`w-[117px] h-[149px] lg:w-[190px] lg:h-[242px]`}>
           <Picture
             src='https://res.cloudinary.com/dwqpulybv/image/upload/v1694399409/shared-assets/landing%20page/81cfb4647d0ce8bc8c7ecba878032d86_vr16x1.png'
             className='rounded-[10px] rounded-bl-[40px]'
@@ -33,16 +33,16 @@ function Gallery({}: Props) {
         </div>
       </div>
 
-      <div className={`flex flex-col gap-[12px]`}>
-        <div className={`w-[326px] h-[245px]`}>
+      <div className={`flex flex-col gap-[7px] lg:gap-[12px]`}>
+        <div className={`w-[201px] h-[151px] lg:w-[326px] lg:h-[245px]`}>
           <Picture
             src='https://res.cloudinary.com/dwqpulybv/image/upload/v1694312742/shared-assets/landing%20page/a91fee560b1323924e7595bc0451342e_kf2j9y.png'
             className='rounded-[10px] rounded-tr-[40px]'
           />
         </div>
-        <div className={`flex gap-[12px]`}>
-          <div className={`flex flex-col items-center gap-[12px]`}>
-            <div className={`w-[190px] h-[126px]`}>
+        <div className={`flex gap-[7px] lg:gap-[12px]`}>
+          <div className={`flex flex-col items-center gap-[7px] lg:gap-[12px]`}>
+            <div className={`w-[117px] h-[77px] lg:w-[190px] lg:h-[126px]`}>
               <Picture
                 src='https://res.cloudinary.com/dwqpulybv/image/upload/v1694399430/shared-assets/landing%20page/a3a1450316d5a94be23392a7fae3553d_v0rlay.png'
                 className='rounded-[10px]'
@@ -54,10 +54,11 @@ function Gallery({}: Props) {
               src='https://res.cloudinary.com/dwqpulybv/image/upload/v1694399673/shared-assets/landing%20page/pic1_ogwnd4.svg'
               width={152}
               height={21}
+              className='w-[104px] h-[14px] lg:w-[152px] lg:h-[21px]'
             />
           </div>
 
-          <div className={`w-[125px] h-[166px]`}>
+          <div className={`w-[77px] h-[102px] lg:w-[125px] lg:h-[166px]`}>
             <Picture
               src='https://res.cloudinary.com/dwqpulybv/image/upload/v1694399435/shared-assets/landing%20page/1bc3d41765f43d5bce66d2fa3ad06a99_mazob9.png'
               className='rounded-[10px]'
--- components/Home/Welcome/Welcome.tsx
+++ components/Home/Welcome/Welcome.tsx
@@ -7,19 +7,19 @@ type Props = {}
 
 const Welcome = (props: Props) => {
   return (
-    <section className='px-[--root-margin] py-[34px] flex items-center justify-between relative'>
+    <section className='px-[--root-margin] pt-[43px] pb-[80px] lg:pt-[34px] lg:pb-[100px] flex flex-col lg:flex-row gap-[60px] lg:gap-0 items-center lg:justify-between relative'>
       <div>
         <div
-          className={`px-[10px] py-[17px] w-fit text-primary bg-primary_light text-[14px] font-[500] rounded-[5px] leading-[120%]`}>
+          className={`px-[10px] py-[17px] w-fit text-primary bg-primary_light text-[12px] lg:text-[14px] font-[500] rounded-[5px] leading-[120%]`}>
           WORKING TOGETHER WITH MEDICINE
         </div>
         <div
-          className={`w-[459px] mt-[12px] mb-[20px] text-[36px] font-[600] `}>
+          className={`w-[306px] lg:w-[459px] mt-[8px] lg:mt-[12px] mb-[20px] text-[24px] lg:text-[36px] font-[600] `}>
           Enabling Worldwide Access to Personalized Precision{' '}
           <span className={`text-primary`}>Medicine</span>
         </div>
         <div
-          className={`mb-[32px] w-[414px] text-gray_1 text-[18px] leading-[120%]`}>
+          className={`w-[303px] lg:w-[414px] text-gray_1 text-[14px] lg:text-[18px] leading-[120%]`}>
           Welcome to OneDrug, where we&apos;re revolutionizing healthcare with
           cutting-edge smart technology solutions
         </div>
